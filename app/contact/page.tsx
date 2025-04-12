e5577e17fe861439ecba7e3a0515c9f9b3841330
MateusCassuque <mateusabril7@gmail.com>
2025-04-12 11:00:38 +0000

    Killing contact import error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { Mail, Phone, MapPin, Backpack, ArrowBigLeft } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import ContactForm from '@/components/contact-form.tsx'
+import ContactForm from '@/components/contact-form'
 
 export default function Contact() {
   return (
